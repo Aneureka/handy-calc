bd415ddefc4b348e51e0e75d3f0d4dfbc39d8224
Hiki <aneureka2@gmail.com>
2020-03-09 22:48:25 +0800

    Add latex and asciimath support

--- app/service/convert.py
+++ app/service/convert.py
@@ -7,12 +7,19 @@ from flask import current_app
 
 def convert_image_to_latex(image_uri=None):
     if image_uri is None:
-      # test mode
-      file_path = '.tmp/test.jpg'
-      image_uri = "data:image/jpg;base64," + base64.b64encode(open(file_path, 'rb').read()).decode()
+        # test mode
+        file_path = '.tmp/test3.jpg'
+        image_uri = "data:image/jpg;base64," + base64.b64encode(open(file_path, 'rb').read()).decode()
     resp = requests.post(
         url=current_app.config['MATHPIX_API'],
-        data=json.dumps({'src': image_uri}),
+        data=json.dumps({
+          'src': image_uri,
+          'formats': ['text', 'data'],
+          'data_options': {
+            'include_latex': True,
+            'include_asciimath': True
+          } 
+        }),
         headers={
             'app_id': current_app.config['MATHPIX_APP_ID'],
             'app_key': current_app.config['MATHPIX_APP_KEY'],
