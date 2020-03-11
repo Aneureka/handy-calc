4831a2567ea4bfe56e914aef5216d8b532ecb6a8
Shenmiu <gcm971016@gmail.com>
2020-03-11 23:14:35 +0800

    feat: latex2sympy then calculate the result

--- app/service/convert.py
+++ app/service/convert.py
@@ -1,8 +1,9 @@
-import sys
 import base64
-import requests
 import json
+
+import requests
 from flask import current_app
+from latex2sympy_custom4.process_latex import process_sympy
 
 
 def convert_image_to_latex(image_uri=None):
@@ -13,12 +14,12 @@ def convert_image_to_latex(image_uri=None):
     resp = requests.post(
         url=current_app.config['MATHPIX_API'],
         data=json.dumps({
-          'src': image_uri,
-          'formats': ['text', 'data'],
-          'data_options': {
-            'include_latex': True,
-            'include_asciimath': True
-          } 
+            'src': image_uri,
+            'formats': ['text', 'data'],
+            'data_options': {
+                'include_latex': True,
+                'include_asciimath': True
+            }
         }),
         headers={
             'app_id': current_app.config['MATHPIX_APP_ID'],
@@ -28,3 +29,22 @@ def convert_image_to_latex(image_uri=None):
     )
     print(json.dumps(json.loads(resp.text), indent=4, sort_keys=True))
     return resp.text
+
+
+def latex_equation(latex_text):
+    res = calculate(latex_text)
+    equation = latex_text + ' = ' + str('%.2f' % res)
+    print('latex_equation:' + equation)
+    return equation
+
+
+def calculate(latex_text=None):
+    sympy_expr = process_sympy(latex_text)
+
+    res = sympy_expr.evalf()
+    return res
+
+
+if __name__ == '__main__':
+    inputLatex = '\sum_{i = 1}^{10} i'
+    latex_equation(inputLatex)
